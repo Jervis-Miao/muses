ffec4c45c44e00317f02782651d5c60355f8018e
miaoqiang <miaoqiang@xyz.cn>
2018-10-26 09:55:00 +0800

    threadlocal

---
+++ demo-web/src/main/java/com/study/multithread/ThreadLocalTest.java
@@ -0,0 +1,63 @@
+/*
+Copyright 2016 Focus Technology, Co., Ltd. All rights reserved.
+ */
+
+package com.study.multithread;
+
+/**
+ * @author miaoqiang
+ * @date 2018/10/26.
+ */
+public class ThreadLocalTest {
+	/**
+	 * 线程本地变量，每个线程中都创建一个副本，互不干涉，多用于：数据库连接、Session管理等
+	 */
+	public static ThreadLocal<Long>		longLocal	= new ThreadLocal<Long>() {
+														// 重写初始化，即使调用get前不set，也不会空指针异常
+														@Override
+														protected Long initialValue() {
+															return Thread.currentThread().getId();
+														};
+													};
+	public static ThreadLocal<String>	stringLocal	= new ThreadLocal<String>() {
+														@Override
+														protected String initialValue() {
+															return Thread.currentThread().getName();
+														};
+													};
+
+	public void set() {
+		longLocal.set(Thread.currentThread().getId());
+		stringLocal.set(Thread.currentThread().getName());
+	}
+
+	public long getLong() {
+		return longLocal.get();
+	}
+
+	public String getString() {
+		return stringLocal.get();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		final ThreadLocalTest test = new ThreadLocalTest();
+
+		test.set();
+		System.out.println(test.getLong());
+		System.out.println(test.getString());
+
+		Thread thread1 = new Thread() {
+			@Override
+			public void run() {
+				test.set();
+				System.out.println(test.getLong());
+				System.out.println(test.getString());
+			}
+		};
+		thread1.start();
+		thread1.join();
+
+		System.out.println(test.getLong());
+		System.out.println(test.getString());
+	}
+}
