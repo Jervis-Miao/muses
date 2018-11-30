6c603c540b7a861680e87a7f4099201cc6b52686
miaoqiang <miaoqiang@xyz.cn>
2018-11-30 11:22:23 +0800

    add mvc

---
+++ demo-web/src/main/java/com/muses/repository/dao/InsureApplicantInfoDAO.java
@@ -0,0 +1,8 @@
+package com.muses.repository.dao;
+
+import com.muses.common.orm.mybatis.MyBatisRepository;
+import com.repository.mapper.InsureApplicantInfoMapper;
+
+@MyBatisRepository
+public interface InsureApplicantInfoDAO extends InsureApplicantInfoMapper {
+}
